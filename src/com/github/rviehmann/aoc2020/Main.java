62ca7b746ae5c8bcd591feed245650bcd72c18b0
rviehmann <rolf.viehmann@googlemail.com>
2020-12-05 00:28:15 +0100

    My solutions for https://adventofcode.com/2020/ so far, the following is already working: * Days 01-02 (completely)

---
+++ src/com/github/rviehmann/aoc2020/Main.java
@@ -0,0 +1,14 @@
+package com.github.rviehmann.aoc2020;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("### Solutions ###");
+        System.out.println("Day 01, puzzle 1: " + Day01.doPuzzle1());
+        System.out.println("Day 01, puzzle 2: " + Day01.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 02, puzzle 1: " + Day02.doPuzzle1());
+        System.out.println("Day 02, puzzle 2: " + Day02.doPuzzle2());
+    }
+}
