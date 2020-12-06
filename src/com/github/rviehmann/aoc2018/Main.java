d7e96f5906fb8ae1a67c5cb3697669baf54c055b
rviehmann <rolf.viehmann@googlemail.com>
2020-12-06 02:11:23 +0100

    Attempted solution to day 05, but result seems to be wrong for an unknown reason.

---
+++ src/com/github/rviehmann/aoc2018/Main.java
@@ -0,0 +1,23 @@
+package com.github.rviehmann.aoc2018;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("### Solutions ###");
+        System.out.println("Day 01, puzzle 1: " + Day01.doPuzzle1());
+        System.out.println("Day 01, puzzle 2: " + Day01.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 02, puzzle 1: " + Day02.doPuzzle1());
+        System.out.println("Day 02, puzzle 2: " + Day02.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 03, puzzle 1: " + Day03.doPuzzle1());
+        System.out.println("Day 03, puzzle 2: " + Day03.doPuzzle2());
+
+        Day05.testWithExamplesForPuzzle1();
+        System.out.println("### Solutions ###");
+        System.out.println("Day 05, puzzle 1: " + Day05.doPuzzle1());
+        System.out.println("Day 05, puzzle 2: " + Day05.doPuzzle2());
+    }
+}
