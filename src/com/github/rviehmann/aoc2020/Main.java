3ff2ca70b4213bd496ff771b4775dcbef7c4d32d
rviehmann <rolf.viehmann@googlemail.com>
2020-12-06 22:48:15 +0100

    Finished day 06 of 2020.

--- src/com/github/rviehmann/aoc2020/Main.java
+++ src/com/github/rviehmann/aoc2020/Main.java
@@ -23,5 +23,9 @@ public class Main {
         System.out.println("### Solutions ###");
         System.out.println("Day 05, puzzle 1: " + Day05.doPuzzle1());
         System.out.println("Day 05, puzzle 2: " + Day05.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 06, puzzle 1: " + Day06.doPuzzle1());
+        System.out.println("Day 06, puzzle 2: " + Day06.doPuzzle2());
     }
 }
