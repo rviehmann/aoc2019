b1a81654260a726da05b6e0bebf31156b9165551
rviehmann <rolf.viehmann@googlemail.com>
2020-12-27 17:29:57 +0100

    Finished day 25 (which has only one puzzle).

---
+++ src/com/github/rviehmann/aoc2020/Day25.java
@@ -0,0 +1,56 @@
+package com.github.rviehmann.aoc2020;
+
+public class Day25 {
+
+    /**
+     * The subject is given in the assignment for this day, and seems to be the same for everyone.
+     */
+    private static final long SUBJECT = 7;
+
+    /**
+     * The divider is given in the assignment for this day, and seems to be the same for everyone.
+     */
+    private static final long DIVIDER = 20201227;
+
+    private static long getLoopSize(long publicKey) {
+        long loop = 0;
+        long value = 1;
+        while (true) {
+            value *= SUBJECT;
+            value %= DIVIDER;
+            loop++;
+            if (value == publicKey) {
+                return loop;
+            }
+        }
+    }
+
+    private static long determineEncryptionKey(long subject, long loopSize) {
+        long value = 1;
+        for (long l = 0; l < loopSize; l++) {
+            value *= subject;
+            value %= DIVIDER;
+        }
+        return value;
+    }
+
+    public static void testWithExamplesForPuzzle1() {
+        System.out.println("### Day 25: Examples for puzzle 1 ###");
+        System.out.println("This should be 8 (card): " + getLoopSize(5764801));
+        System.out.println("This should be 11 (door): " + getLoopSize(17807724));
+        System.out.println("This should be the key (14897079): " + determineEncryptionKey(17807724, 8));
+        System.out.println("This should be the key (14897079): " + determineEncryptionKey(5764801, 11));
+    }
+
+    public static long doPuzzle1() {
+        long loopCard = getLoopSize(1717001);
+        long loopDoor = getLoopSize(523731);
+        System.out.println("We will need this loop size (card): " + loopCard);
+        System.out.println("We will need this loop size (door): " + loopDoor);
+        System.out.println("This should be the key: " + determineEncryptionKey(17807724, loopCard));
+        System.out.println("This should be the key: " + determineEncryptionKey(1717001, loopDoor));
+        return determineEncryptionKey(1717001, loopDoor);
+    }
+
+    // There is not puzzle 2 for this day.
+}
