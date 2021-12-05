7f1cb39cc6b5d1e1d31a5348bf1a5de1e45ae94e
Rolf Viehmann <rolf.viehmann@klarna.com>
2021-12-05 22:42:06 +0100

    Glue logic for 2021.

---
+++ src/com/github/rviehmann/aoc2021/Main.java
@@ -0,0 +1,17 @@
+package com.github.rviehmann.aoc2021;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("### Solutions ###");
+        System.out.println("Day 01, puzzle 1: " + Day01.doPuzzle1());
+        System.out.println("Day 01, puzzle 2: " + Day01.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 02, puzzle 1: " + Day02.doPuzzle1());
+        System.out.println("Day 02, puzzle 2: " + Day02.doPuzzle2());
+
+        System.out.println("### Solutions ###");
+        System.out.println("Day 03, puzzle 1: " + Day03.doPuzzle1());
+    }
+}
