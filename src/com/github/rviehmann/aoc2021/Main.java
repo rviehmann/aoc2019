fda7725b4bc4cc4140aaa981a71cf3b3deb395ff
Rolf Viehmann <rolf.viehmann@klarna.com>
2022-12-04 23:06:42 +0100

    Old year (2021): Day 10 first half finished.

--- src/com/github/rviehmann/aoc2021/Main.java
+++ src/com/github/rviehmann/aoc2021/Main.java
@@ -3,6 +3,7 @@ package com.github.rviehmann.aoc2021;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         System.out.println("### Solutions ###");
         System.out.println("Day 01, puzzle 1: " + Day01.doPuzzle1());
         System.out.println("Day 01, puzzle 2: " + Day01.doPuzzle2());
@@ -21,5 +22,11 @@ public class Main {
         System.out.println("### Solutions ###");
         System.out.println("Day 04, puzzle 1: " + Day04.doPuzzle1());
         System.out.println("Day 04, puzzle 2: " + Day04.doPuzzle2());
+        */
+
+        Day10.testWithExamplesForPuzzle1();
+        System.out.println("### Solutions ###");
+        System.out.println("Day 10, puzzle 1: " + Day10.doPuzzle1());
+        // System.out.println("Day 10, puzzle 2: " + Day10.doPuzzle2());
     }
 }
